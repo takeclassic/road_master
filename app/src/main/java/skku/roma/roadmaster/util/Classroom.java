8018e1f5a2916428efd098fb8816783e5bff2766
nyu531 <nyu531@gmail.com>
2016-05-13 03:58:12 +0900

    GPS 외 DB 수정 등

---
+++ app/src/main/java/skku/roma/roadmaster/util/Classroom.java
@@ -0,0 +1,16 @@
+package skku.roma.roadmaster.util;
+
+/**
+ * Created by nyu531 on 2016-05-06.
+ */
+public class Classroom {
+    int number;
+    String name;
+    float weight;
+
+    public Classroom(int number, String name, float weight) {
+        this.number = number;
+        this.name = name;
+        this.weight = weight;
+    }
+}
